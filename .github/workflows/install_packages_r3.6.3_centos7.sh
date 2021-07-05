406b743aa21912022b4ec5e2f96a2d3e2df7ef18
Yuri05 <Yuri05@github.com>
2021-07-05 18:27:16 +0200

    ospsuite-r 10.0.25

--- .github/workflows/install_packages_r3.6.3_centos7.sh
+++ .github/workflows/install_packages_r3.6.3_centos7.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Get software
+# Get software 
 wget -nv https://github.com/Open-Systems-Pharmacology/rClr/releases/download/v$1-R3/R3_rClr_$1_CentOS7.tar.gz -P /tmp_setup/
 wget -nv https://github.com/Open-Systems-Pharmacology/TLF-Library/releases/download/v$2/tlf_$2.tar.gz -P /tmp_setup/
 wget -nv https://github.com/Open-Systems-Pharmacology/OSPSuite-R/releases/download/v$3/ospsuite_$3_centOS7.tar.gz -P /tmp_setup/
