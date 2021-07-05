e5b782fb26f4fa01697c4c9a65537009ea66a266
Yuri05 <Yuri05@github.com>
2021-07-05 18:13:53 +0200

    ospsuite-r 10.0.25

---
+++ .github/workflows/install_packages_r3.6.3_bionic.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Get software
+wget -nv https://github.com/Open-Systems-Pharmacology/rClr/releases/download/v$1-R3/R3_rClr_$1_Ubuntu18.tar.gz -P /tmp_setup/
+wget -nv https://github.com/Open-Systems-Pharmacology/TLF-Library/releases/download/v$2/tlf_$2.tar.gz -P /tmp_setup/
+wget -nv https://github.com/Open-Systems-Pharmacology/OSPSuite-R/releases/download/v$3/ospsuite_$3_ubuntu18.tar.gz -P /tmp_setup/
+
+# Install packages
+R CMD INSTALL /tmp_setup/R3_rClr_$1_Ubuntu18.tar.gz && \
+R CMD INSTALL /tmp_setup/tlf_$2.tar.gz --install-tests && \
+R CMD INSTALL /tmp_setup/ospsuite_$3_ubuntu18.tar.gz --install-tests && \
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Test-Docker-Ospsuite-R
+# Docker-Images
+Image includes installations of:
+* [TLF Library](https://github.com/Open-Systems-Pharmacology/TLF-Library)
+* [OSPSuite-R](https://docs.open-systems-pharmacology.org/working-with-r/r-introduction)
+* [OSP Suite Reporting Engine](https://github.com/Open-Systems-Pharmacology/OSPSuite.ReportingEngine)
